de25e7630e0d0eb3a8ba10cb2a395e2aa5d2ec6e
Gernot Lenkner <g.lenkner@outlook.de>
2019-07-12 14:34:13 +0200

    list all chunks

--- NoteConverter/Program.cs
+++ NoteConverter/Program.cs
@@ -11,7 +11,15 @@ namespace NoteConverter
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Reading MIDI File...");
             var r = MIDIReader.ReadAllChunks("test.mid");
+            Console.WriteLine($"found {r.Count} Chunk(s)...");
+            int i = 1;
+            foreach (MIDIChunk chunk in r)
+            {
+                Console.WriteLine($"Nr. {i}: {chunk.ToString()}");
+                i++;
+            }
         }
     }
 }
