71ea12a82d63d4122df858f5c5d7ff5fa63f2600
Gernot Lenkner <g.lenkner@outlook.de>
2019-07-12 11:34:49 +0200

    initial commit

---
+++ MIDI/MIDIReader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace MIDI
+{
+    public class MIDIReader
+    {
+        public static List<MIDIChunk> ReadAllChunks(string fileName)
+        {
+            using (FileStream stream = File.OpenRead(fileName))
+            {
+                return ReadAllChunks(stream);
+            }
+        }
+
+        public static List<MIDIChunk> ReadAllChunks(Stream stream)
+        {
+            var reader = new MIDIReader(stream);
+            var chunks = new List<MIDIChunk>();
+
+            // reader.Close();
+            return chunks;
+        }
+
+        public MIDIReader(Stream stream)
+        {
+
+        }
+
+    }
+}
---
+++ NoteConverter/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("NoteConverter")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NoteConverter")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("065773c3-9e2a-495f-a1c3-8175ffe8fd56")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
